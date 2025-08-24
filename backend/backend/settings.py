96d2c776e44cdb454a729fa826153f8d0ef2113c
tarun <tarunmedisetti13@gmail.com>
2025-08-24 22:19:04 +0530

    changes in setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "frontend" / "dist"],  # ✅ React build location
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
